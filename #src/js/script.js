05b5654923c999988107b09bbadf53eaa9d8e0a7
Ermolenko Sergey <ermolenko.post.gmail.com>
2021-04-06 16:37:14 +0300

    Block video and mobile.

--- #src/js/script.js
+++ #src/js/script.js
@@ -129,3 +129,26 @@ const gotoNum = number => {
 
 update();
 
+
+// VIDEO FUNCTION
+
+$('#play-video').on('click', function(e){
+    e.preventDefault();
+    $('#video-overlay').addClass('open');
+    $("#video-overlay").append('<iframe width="100%" height="100%" src="https://www.youtube.com/embed/ngElkyQ6Rhs" frameborder="0" allowfullscreen></iframe>');
+});
+
+$('.video-overlay, .video-overlay-close').on('click', function(e){
+    e.preventDefault();
+    close_video();
+});
+
+$(document).keyup(function(e){
+    if(e.keyCode === 27) { close_video(); }
+});
+
+function close_video() {
+    $('.video-overlay.open').removeClass('open').find('iframe').remove();
+};
+
+
