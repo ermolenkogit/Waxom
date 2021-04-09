40d18402891ac9f9938af7ad315aea63bc9862c0
Ermolenko Sergey <ermolenko.post.gmail.com>
2021-04-09 10:31:50 +0300

    Block benefits.

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ let path = {
     src: {
         html: [source_folder + "/*.html", `!${source_folder}/_*.html`],
         css: source_folder + "/scss/style.scss",
-        js: source_folder + "/js/script.js",
+        js: source_folder + "/js/**/*.js",      // было "/js/script.js"
         img: source_folder + "/img/**/*.{png,jpg,gif,ico,svg,webp}",
     },
     watch: {
